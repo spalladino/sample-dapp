77006d0351a9c2f5017f1b051f6b88a4ab4364aa
Santiago Palladino <spalladino@gmail.com>
2018-12-23 19:58:17 -0300

    Updates

---
+++ precache-manifest.3501ce2e398e8294feaa65e09f1b8a57.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8d30ee7e30f95e4b6f3e",
+    "url": "/static/js/main.8d30ee7e.chunk.js"
+  },
+  {
+    "revision": "ccbb36f49544b8a3ade5",
+    "url": "/static/js/1.ccbb36f4.chunk.js"
+  },
+  {
+    "revision": "8d30ee7e30f95e4b6f3e",
+    "url": "/static/css/main.1b43dffb.chunk.css"
+  },
+  {
+    "revision": "7cb2972bbe483a42314159024dd2e334",
+    "url": "/index.html"
+  }
+];
