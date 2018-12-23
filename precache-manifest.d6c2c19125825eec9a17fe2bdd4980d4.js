67e49257b9be378b7743f1501c176a0782a270e0
Santiago Palladino <spalladino@gmail.com>
2018-12-23 20:09:09 -0300

    Updates

---
+++ precache-manifest.d6c2c19125825eec9a17fe2bdd4980d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3ff2094d6585f523d1bc",
+    "url": "/sample-dapp/static/js/runtime~main.3ff2094d.js"
+  },
+  {
+    "revision": "3e1b906ad70ec18ec6e5",
+    "url": "/sample-dapp/static/js/main.3e1b906a.chunk.js"
+  },
+  {
+    "revision": "ccbb36f49544b8a3ade5",
+    "url": "/sample-dapp/static/js/1.ccbb36f4.chunk.js"
+  },
+  {
+    "revision": "3e1b906ad70ec18ec6e5",
+    "url": "/sample-dapp/static/css/main.1b43dffb.chunk.css"
+  },
+  {
+    "revision": "36266f9b6ce0c1106aa9fc36e211d479",
+    "url": "/sample-dapp/index.html"
+  }
+];
